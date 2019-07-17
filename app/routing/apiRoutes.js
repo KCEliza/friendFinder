08c6fc2bd5de854ee578f58b86d4b72181cf9862
KCEliza <48337639+KCEliza@users.noreply.github.com>
2019-07-17 13:31:46 -0600

    for loop getting through friends

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,48 @@
+var friends = require("../data/friends.js");
+
+
+//api get request
+module.exports = function (app) {
+    app.get("/api.friends", function (req, res) {
+        res.json(friends);
+    });
+    app.post("/api/friends", function (req, res) {
+        var totalDifference = 0;
+        var bestMatch = {
+            name: "",
+            photo: "",
+            friendDifference: 1000
+        };
+        var userData = req.body;
+        var userName = userData.name;
+        var userScores = userData.scores;
+
+        var b = userScores.map(function (item) {
+            return parseInt(item, 10);
+        });
+        userData = {
+            name: req.body.name,
+            photo: req.body.photo,
+            scores: b
+        }
+        var sum = b.reduce((a, b) => a+b, 0)
+        console.log(`Name: ${name}\nUser Score: ${userScores}\n`);
+        console.log(`Sum of user score ${sum}\nBest friend match ${bestMatch.friendDifference}`);
+    
+
+        for(var i = 0; i < friends.length; i++){
+            console.log(friends[i].name);
+            totalDifference = 0;
+            console.log(`Total Difference: ${totalDifference}\nBest friend match: ${bestMatch.friendDifference}`)
+        
+        var bFriendScore = friends[i].scores.reduce ((a,b) => a+b, 0)
+        console.log(`Total friend score: ${bFriendScore}`);
+        totalDifference += Math.abs(sum - bFriendScore);
+        console.log(totalDifference);
+
+        }
+    });
+
+
+
+};
---
+++ app/server.js
@@ -0,0 +1,24 @@
+
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
+
+
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.text());
+app.use(bodyParser.json({type: "application/vnd.api+json"}));
+
+app.use(express.static("app/public"));
+
+require("./app/routing/api-routes.js")(app);
+require("./app/routing/html-routes.js")(app);
+
+
+// Start listening on PORT
+app.listen(PORT, function() {
+  console.log('Friend Finder app is listening on PORT: ' + PORT);
+});
