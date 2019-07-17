20e848fd99eed3a0d088ef099f9591521504e077
KCEliza <48337639+KCEliza@users.noreply.github.com>
2019-07-17 13:17:08 -0600

    routes almost done

---
+++ app/data/friends.js
@@ -0,0 +1,37 @@
+var friends = [{
+      name: "Ahmed",
+      photo: "https://media.licdn.com/mpr/mpr/shrinknp_200_200/AAEAAQAAAAAAAAq7AAAAJDAwYzI4NTQ4LWYwZWUtNGFkYS1hNTYwLTZjYzkwY2ViZDA3OA.jpg",
+      scores: [5, 2, 3, 1, 4, 3, 3, 1, 1, 5]
+    },
+    {
+      name: "Jacob Deming",
+      photo: "https://pbs.twimg.com/profile_images/691785039043022849/oWsy8LNR.jpg",
+      scores: [2, 1, 5, 1, 5, 2, 4, 5, 1, 3]
+    },
+    {
+      name: "Jeremiah Scanlon",
+      photo: "https://avatars2.githubusercontent.com/u/8504998?v=3&s=460",
+      scores: [2, 4, 1, 5, 5, 5, 2, 2, 2, 3]
+    },
+    {
+      name: "Louis T. Delia",
+      photo: "https://pbs.twimg.com/profile_images/639214960049000449/lNCRC-ub.jpg",
+      scores: [5, 5, 2, 2, 4, 5, 1, 3, 3, 4]
+    },
+    {
+      name: "Lou Ritter",
+      photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAkDAAAAJDhhZTI5NTk2LWQzZjUtNDJjZi1hMTM2LTQ3ZjNmYjE0YmY2NA.jpg",
+      scores: [4, 5, 1, 4, 3, 5, 1, 5, 3, 3]
+    },
+    {
+      name: "Jordan Biason",
+      photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAisAAAAJGUxYzc4YzA0LWQxMzUtNGI4NS04YTFiLTkwYzM0YTZkNzA2NA.jpg",
+      scores: [4, 1, 3, 3, 1, 2, 3, 4, 1, 5]
+    },
+    {
+      name: "Ellen Evans",
+      photo: "https://media.licdn.com/dms/image/C4E03AQEsi5hX-3HgFA/profile-displayphoto-shrink_200_200/0?e=1568851200&v=beta&t=VHOMTOWnyZ6U1CTu0RmruPqKodDPSlDNZIy-IaUcq9g",
+      scores: [2, 5, 3, 1, 1, 2, 1, 5, 2, 3]
+    }];
+
+    module.exports = friends;
