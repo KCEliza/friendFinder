649df60174404daeb7851bddec58cec52abde646
KCEliza <48337639+KCEliza@users.noreply.github.com>
2019-07-20 10:02:00 -0600

    working except images

--- server.js
+++ server.js
@@ -11,8 +11,8 @@ app.use(express.json());
 
 app.use(express.static("app/public"));
 
-require("app/routing/apiRoutes.js")(app);
-require("app/routing/htmlRoutes.js")(app);
+require("./app/routing/apiRoutes.js")(app);
+require("./app/routing/htmlRoutes.js")(app);
 
 
 // Start listening on PORT
