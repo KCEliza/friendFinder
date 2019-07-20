ed533ce62795c77327a84529c7e8c4a28edf22c4
KCEliza <48337639+KCEliza@users.noreply.github.com>
2019-07-20 14:22:09 -0600

    final

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,36 +1,36 @@
 var friends = [{
       name: "Ahmed",
-      photo: "images/pupper.jpg",
+      photo: "https://www.noao.edu/image_gallery/images/d2/ngc2237_400.jpg",
       scores: [5, 2, 3, 1, 4, 3, 3, 1, 1, 5]
     },
     {
       name: "Jacob Deming",
-      photo: "../images/fireball.jpg",
+      photo: "https://www.guidelive.com/resources/motif/images/header-mad-libs/doing/anything_400x400.jpg",
       scores: [2, 1, 5, 1, 5, 2, 4, 5, 1, 3]
     },
     {
       name: "Jeremiah Scanlon",
-      photo: "../images/cameraGuy.jpg",
+      photo: "https://www.auntminnie.com/user/images/content_images/pho_redir/2019_06_11_22_35_5932_heart_pulmonary_trunk_400.jpg",
       scores: [2, 4, 1, 5, 5, 5, 2, 2, 2, 3]
     },
     {
       name: "Louis T. Delia",
-      photo: "../images/pupper.jpg",
+      photo: "https://news.prairiepublic.org/sites/ndpr/files/201906/Apologies-400x400.jpg",
       scores: [5, 5, 2, 2, 4, 5, 1, 3, 3, 4]
     },
     {
       name: "Lou Ritter",
-      photo: "../images/cliffGuy.jpg",
+      photo: "http://greenleafcare.org/wp-content/uploads/2019/06/xcodebluedabtaffy-400x400.jpg.pagespeed.ic._hud2poEN6.jpg",
       scores: [4, 5, 1, 4, 3, 5, 1, 5, 3, 3]
     },
     {
       name: "Jordan Biason",
-      photo: "../images/waterGirl.jpeg",
+      photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTIPso8URMRucY1CSFNGc-lNj43qKtR-OcXsQTTDzRLoy2NJ6dhcg",
       scores: [4, 1, 3, 3, 1, 2, 3, 4, 1, 5]
     },
     {
       name: "Ellen Evans",
-      photo: "../images/einstein.webp",
+      photo: "https://douglascuddletoy.com/wp-content/uploads/2019/01/2420-400x400.jpg",
       scores: [2, 5, 3, 1, 1, 2, 1, 5, 2, 3]
     }];
 
